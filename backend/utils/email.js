1d6427c0ba3cd2664e1dffdbb10746ed6498508c
JohnnieKay12 <142480261+JohnnieKay12@users.noreply.github.com>
2026-05-07 14:30:20 +0100

    Fixed Gmail email transporter

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -3,15 +3,10 @@ const nodemailer = require('nodemailer');
 // Create transporter
 const createTransporter = () => {
   return nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: false,
+    service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
-    },
-    tls: {
-      rejectUnauthorized: false
     }
   });
 };
